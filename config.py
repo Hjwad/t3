f5664b29615ef4cbf092b8fe85a099e8c9c5e16e
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-04-15 23:05:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Hjwad/y",
+    "https://github.com/Hjwad/t3",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
